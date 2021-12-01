99060b095c2bbd49b9849eb39f865877a9e432ae
mau <9014816@student.zadkine.nl>
2021-12-01 17:30:26 +0100

    aangepast

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
 <?php
 echo "dit is de index pagina";
-
+?>
+<head>
+    <meta charset="UTF-8">
+    <title>  </title>
+</head>
+<body>
+    <h1>garage menu</h1>
+    <h2>klant</h2>
+    <ul>
+        <li><a href="create-klant.php">create</a> </li>
+        <li><a href="read-klant.php">read</a> </li>
+        <li><a href="search-klant.php">search</a> </li>
+        <li><a href="update-klant.php">update</a> </li>
+        <li><a href="delete-klant.php">delete</a> </li>
+    </ul>
+</body>
