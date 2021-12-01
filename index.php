2ae3267606b95d9331005fac7cdd29a53bfe4acf
mau <9014816@student.zadkine.nl>
2021-12-01 18:47:32 +0100

    list added

--- index.php
+++ index.php
@@ -15,4 +15,12 @@ echo "dit is de index pagina";
         <li><a href="update-klant.php">update</a> </li>
         <li><a href="delete-klant.php">delete</a> </li>
     </ul>
+    <h2>auto</h2>
+    <ul>
+        <li><a href="create-auto.php">create</a> </li>
+        <li><a href="read-auto.php">read</a> </li>
+        <li><a href="search-auto.php">search</a> </li>
+        <li><a href="update-auto.php">update</a> </li>
+        <li><a href="delete-auto.php">delete</a> </li>
+    </ul>
 </body>
