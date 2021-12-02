053265dfdf894cf0f5fb847387465c10ca04ee47
mau <9014816@student.zadkine.nl>
2021-12-02 10:24:11 +0100

    create klant

--- create-klant.php
+++ create-klant.php
@@ -1,3 +1,15 @@
 <?php include 'functions.php';
 ?>
 <h1>garage create klant</h1>
+<p>
+    Dit formulier wordt gebruikt om klantgegevens in te voeren.
+</p>
+<form action="create-klant.php" method="post">
+    klantID: <input type="text" name="klantIDvak">
+    klantnaam: <input type="text" name="klantnaamvak">
+    klantadres: <input type="text" name="klantadresvak">
+    klantpostcode: <input type="text" name="klantpostcodevak">
+    klantE-mail: <input type="text" name="klantE-mailsvak">
+    klanttelefoonnummer: <input type="text" name="klanttelefoonnummersvak">
+    <input type="submit">
+</form>
