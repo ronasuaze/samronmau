6d95bafaa3c793e0d9951dfa8b9281c8b180e545
maurino <9014816@student.zadkine.nl>
2022-01-14 14:14:44 +0100

    .

--- connectie.php
+++ connectie.php
@@ -1,15 +1,15 @@
 <?php
-$servername = "localhost";
-$dbname = "garage ertan";
-$username = "root";
-$password = "root";
+    $servername = "localhost";
+    $dbname = "garage_ertan";
+    $username = "root";
+    $password = "root";
 
-try {
-    $conn = new PDO("mysql:host=$servername;dbname=$dbname",$username,$password);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connectie met de database is gelukt! <br/>";
+    try {
+        $conn = new PDO("mysql:host=$servername;dbname=$dbname",$username,$password);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        echo "Connectie met de database is gelukt! <br/>";
 }
-catch(PDOException $e) {
-    echo "Connectie is mislukt: " . $e->getMessage() . " :( ";
+    catch(PDOException $e) {
+        echo "Connectie is mislukt: " . $e->getMessage() . " :( ";
 }
 ?>
